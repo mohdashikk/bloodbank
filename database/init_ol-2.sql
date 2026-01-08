bab436e0d156260b4662aa770190427950cb5f35
mohdashikk <mashik.a9@gmail.com>
2026-01-08 19:04:47 +0530

    Implemented profile updation

--- database/init_ol-2.sql
+++ database/init_ol-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Jan 08, 2026 at 01:27 PM
+-- Generation Time: Jan 06, 2026 at 12:42 PM
 -- Server version: 8.0.44
 -- PHP Version: 8.3.26
 
@@ -37,9 +37,9 @@ CREATE TABLE `users` (
   `gender` enum('male','female','other') DEFAULT NULL,
   `last_donate_date` date DEFAULT NULL,
   `role` varchar(20) DEFAULT NULL,
-  `is_approved` tinyint(1) NOT NULL DEFAULT '0',
+  `is_approved` tinyint(1) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -47,10 +47,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `phone`, `address`, `blood_group`, `gender`, `last_donate_date`, `role`, `is_approved`, `password`, `created_at`) VALUES
-(1, 'Muhammed', 'mashik.a9@gmail.com', NULL, 'Ashik house thevalakkara', 'A+', 'male', '2026-01-08', 'admin', 0, '$2b$10$qRc0.v7yiMav29Jvjxw8p.xUD6Z9kyXG8ZdgiIegQbA7jMhIb6BQG', '2026-01-08 13:25:22'),
+(1, 'Muhammed Ashik A', 'mashik.a9@gmail.com', NULL, NULL, NULL, NULL, NULL, 'admin', 0, '$2b$10$qRc0.v7yiMav29Jvjxw8p.xUD6Z9kyXG8ZdgiIegQbA7jMhIb6BQG', '2025-12-27 15:40:15'),
 (2, 'Sneha', 'Sneha jo', NULL, NULL, NULL, NULL, NULL, 'user', 1, '$2b$10$JDDlEDV2UtAEkw.g7Pt0D.qF7pjezYAubypjTCyW5a8/uwc4eCVl.', '2025-12-28 06:09:42'),
 (3, 'Muhammed Ashik A', 'mail4ash@gmail.com', NULL, NULL, NULL, NULL, NULL, 'user', 1, '$2b$10$IAKSkarJoj0W/BWVrJbUfulwODIll1wMbZk6qlSIoGYBzvl.UwB6a', '2025-12-29 09:21:56'),
-(8, 'Sarah', 'johm@gmail.com', '9656757747', 'Trinity height 609', 'A+', 'male', '2026-01-08', 'user', 0, '$2b$10$RiPeBeL4lIEkxLuPPcPpj.5aBujlgExnFob5cMYLG5oVAft9Zt2xi', '2026-01-08 10:24:51');
+(4, 'Sarah', 'sarah@gmail.com', NULL, NULL, NULL, NULL, NULL, 'user', 0, '$2b$10$19qDtDhm3NNRXRyXO64gy.jqAA0y15fwB5J57D/r09b/9rbx8AEJC', '2025-12-29 11:25:55');
 
 --
 -- Indexes for dumped tables
@@ -71,7 +71,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
