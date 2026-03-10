ec15175086d7b505463e7dfc72e96e0a44f68c59
mohdashikk <mashik.a9@gmail.com>
2026-03-10 15:08:45 +0530

    Approve issue resolved

--- backend/routes/fetchRoutes.js
+++ backend/routes/fetchRoutes.js
@@ -4,9 +4,9 @@ import { protect, admin } from "../middleware/middleware.js";
 
 const route = express.Router();
 
-route.use("/users", protect, fetchData);
-route.use("/single-user", protect, fetchUser)
+route.get("/users", protect, fetchData);
+route.get("/single-user", protect, fetchUser)
 
-route.post("/approve/:id", protect, admin, approveUser);
+route.patch("users/approve/:id", protect, admin, approveUser);
 
 export default route;
